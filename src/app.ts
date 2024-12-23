de3da3451b69f5ec317d3cb451e661c059b14ff0
AbdulKhaliq59 <kananuraabdulkhaliq59@gmail.com>
2024-12-23 12:13:06 +0200

    feat: adding Authentication feature

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
-app.use("/api/v1/auth", authRouter); // Register the auth routes for Google OAuths
+app.use("/api/v1/auth", authRouter);
 app.get("/api/v1", (_req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to qew code backend!",
