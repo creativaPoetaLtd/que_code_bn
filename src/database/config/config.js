9db7b47f7ac1a06ff26bb195c20dbe66eb7c46c9
Abdulkhaliq59 <kananuraabdulkhaliq59@gmail.com>
2025-07-30 18:28:37 +0200

    fix: resolve migrations by couple of files

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,15 +3,21 @@ require("dotenv").config();
 process.env.DB_HOSTED_MODE == "local"
 	? (dialect_option = {})
 	: (dialect_option = {
-			ssl: {
-				require: process.env.SSL,
-				rejectUnauthorized: true,
-			},
-		});
+		ssl: {
+			require: process.env.SSL,
+			rejectUnauthorized: true,
+		},
+	});
 module.exports = {
 	development: {
-		url: process.env.DB_DEV_URL,
-		dialectOptions: dialect_option,
+		use_env_variable: "DB_DEV_URL",
+		dialect: "postgres",
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 	test: {
 		url: process.env.DB_TEST_URL,
--- src/database/migrations/20241105142934-create-user.js
+++ src/database/migrations/20241105142934-create-user.js
@@ -9,60 +9,49 @@ module.exports = {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
       },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      gender: {
-        type: Sequelize.STRING
-      },
-      phone: {
-        type: Sequelize.STRING
-      },
-      province: {
-        type: Sequelize.STRING
-      },
-      district: {
-        type: Sequelize.STRING
-      },
-      sector: {
-        type: Sequelize.STRING
-      },
-      national_id: {
-        type: Sequelize.STRING
-      },
-      approvalStatus: {
-        type: Sequelize.STRING
-      },
-      resetTokenExpires: {
-        type: Sequelize.DATE
-      },
-      resetToken: {
-        type: Sequelize.STRING
-      },
-      isVerified: {
-        type: Sequelize.BOOLEAN
-      },
-      lastOtpSent: {
-        type: Sequelize.DATE
-      },
-      otp: {
-        type: Sequelize.STRING
-      },
+      firstName: { type: Sequelize.STRING },
+      lastName: { type: Sequelize.STRING },
+      email: { type: Sequelize.STRING },
+      password: { type: Sequelize.STRING },
+      gender: { type: Sequelize.STRING },
+      phone: { type: Sequelize.STRING },
+      province: { type: Sequelize.STRING },
+      district: { type: Sequelize.STRING },
+      sector: { type: Sequelize.STRING },
+      national_id: { type: Sequelize.STRING },
+      approvalStatus: { type: Sequelize.STRING },
+      resetTokenExpires: { type: Sequelize.DATE },
+      resetToken: { type: Sequelize.STRING },
+      isVerified: { type: Sequelize.BOOLEAN },
+      lastOtpSent: { type: Sequelize.DATE },
+      otp: { type: Sequelize.STRING },
       qrCode: {
         type: Sequelize.TEXT,
         allowNull: true,
       },
-      otpExpires: {
-        type: Sequelize.DATE
+      otpExpires: { type: Sequelize.DATE },
+      profileImage: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      statusMessage: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      showPhoneOnWelcome: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      showProfileImageOnWelcome: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      showStatusMessageOnWelcome: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
       },
       createdAt: {
         allowNull: false,
@@ -74,6 +63,7 @@ module.exports = {
       }
     });
   },
+
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
   }
