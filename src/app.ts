0b96fc6e8b318b965c63381a322811ee6ea2e449
Yvette IZANYIBUKA <izanyibukayvette@gmail.com>
2026-02-16 17:09:00 +0200

    admin analytics on que code

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,7 @@ app.use(
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://que-code-admin.netlify.app/",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
